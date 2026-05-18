c88d9a55a51c0656fee106facabc38a9e1230a2f
Adelodunpeter25 <adelodunpeter69@gmail.com>
2026-05-18 20:47:12 +0100

    Configure root repository and clean up build artifacts

--- cmdp/.build/x86_64-apple-macosx/debug/CLibSearch.build/empty.c.d
+++
@@ -1,3 +0,0 @@
-dependencies: \
-  /Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX14.2.sdk/usr/include/module.modulemap \
-  /Users/techclub/Public/projects/cmdp/cmdp/Sources/CLibSearch/empty.c
