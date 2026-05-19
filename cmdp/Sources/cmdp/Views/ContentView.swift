436448cb89f21f0e8bbccb1905631f767f1a0df3
Adelodunpeter25 <adelodunpeter69@gmail.com>
2026-05-19 12:38:43 +0100

    style: change command mode icon to chevron.right.2

--- cmdp/Sources/cmdp/Views/ContentView.swift
+++ cmdp/Sources/cmdp/Views/ContentView.swift
@@ -12,7 +12,7 @@ struct ContentView: View {
         VStack(spacing: 0) {
             // Search Bar
             HStack {
-                Image(systemName: searchService.isCommandMode ? "chevron.right.square" : "magnifyingglass")
+                Image(systemName: searchService.isCommandMode ? "chevron.right.2" : "magnifyingglass")
                     .font(.system(size: 22, weight: .light))
                     .foregroundColor(searchService.isCommandMode ? .blue : Theme.searchIconColor)
                     .padding(.leading, 4)
