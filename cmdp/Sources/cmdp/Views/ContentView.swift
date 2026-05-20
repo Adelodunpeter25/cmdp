1a762a360ffb167127ce217d8c1d9c2a5337190b
Adelodunpeter25 <adelodunpeter69@gmail.com>
2026-05-20 11:57:56 +0100

    update command mode icon to apple command icon

--- cmdp/Sources/cmdp/Views/ContentView.swift
+++ cmdp/Sources/cmdp/Views/ContentView.swift
@@ -12,7 +12,7 @@ struct ContentView: View {
         VStack(spacing: 0) {
             // Search Bar
             HStack {
-                Image(systemName: searchService.isCommandMode ? "chevron.right" : "magnifyingglass")
+                Image(systemName: searchService.isCommandMode ? "command" : "magnifyingglass")
                     .font(.system(size: 22, weight: .light))
                     .foregroundColor(Theme.searchIconColor)
                     .padding(.leading, 4)
